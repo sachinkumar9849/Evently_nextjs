e925ca038118bdd7c3110600a6cb5a85eee6fd7c
sachinmadhwa <kjhasachin@gmail.com>
2024-02-25 10:21:15 +0545

    Login and signup completed using Clerk

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,19 +1,20 @@
 import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
+import { ClerkProvider } from "@clerk/nextjs";
 import "./globals.css";
 
 const poppins = Poppins({
   subsets: ["latin"],
   weight: ["400", "500", "600", "700"],
-  variable:"--font-poppins",
+  variable: "--font-poppins",
 });
- 
+
 export const metadata: Metadata = {
   title: "Evently",
   description: "Evently is a platform for event management.",
-  icons:{
-    icon:"/assets/images/logo.svg"
-  }
+  icons: {
+    icon: "/assets/images/logo.svg",
+  },
 };
 
 export default function RootLayout({
@@ -22,8 +23,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={poppins.variable}>{children}</body>
-    </html>
+    <ClerkProvider>
+      <html lang="en">
+        <body className={poppins.variable}>{children}</body>
+      </html>
+    </ClerkProvider>
   );
 }
